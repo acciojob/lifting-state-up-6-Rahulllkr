11024e3a88a33f715b63f91ed9bd54d7adb4f75c
Rahulllkr <rahulkumar8604745@gmail.com>
2026-03-27 17:52:34 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,6 @@ const App = () => {
   return (
     <div>
       <h1>Parent Component</h1>
-      <h1>Parent Component</h1>
         {/* Do not remove the main div */}
         <Child todos={todos} handleComplete={handleComplete}/>
     </div>
